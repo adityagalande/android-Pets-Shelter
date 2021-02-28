62a47ed3874073456e2df700dc9910876f935d89
Aditya Galande <adityagalande14@gmail.com>
2021-02-28 22:50:37 +0530

    cursorLoader handled

--- app/src/main/java/com/example/petsshelter/PetCursorAdapter.java
+++ app/src/main/java/com/example/petsshelter/PetCursorAdapter.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
+import com.example.petsshelter.Data.PetContract;
+
 public class PetCursorAdapter extends CursorAdapter {
     public PetCursorAdapter(Context context, Cursor c) {
         super(context, c, 0);
@@ -22,7 +24,15 @@ public class PetCursorAdapter extends CursorAdapter {
     public void bindView(View view, Context context, Cursor cursor) {
         TextView petName = (TextView) view.findViewById(R.id.pet_name);
         TextView petBreed = (TextView) view.findViewById(R.id.pet_breed);
-        
+
+        int nameColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_NAME);
+        int breedColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_BREED);
+
+        String name = cursor.getString(nameColumnIndex);
+        String breed = cursor.getString(breedColumnIndex);
+
+        petName.setText(name);
+        petBreed.setText(breed);
 
     }
 }
